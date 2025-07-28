3b5a328202e4e21d021dc5935597e75c7e5ebe03
woo-in <wooin0205@naver.com>
2025-07-28 12:16:11 +0900

    deposit 기능에 PRG 패턴 적용(URL 쿼리 기반)

--- src/main/java/bankapp/controller/DepositController.java
+++ src/main/java/bankapp/controller/DepositController.java
@@ -6,10 +6,9 @@ import bankapp.service.BankAccountManager;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
 
 @Slf4j
 @Controller
@@ -33,29 +32,45 @@ public class DepositController {
     @PostMapping("")
     public String processDeposit(@ModelAttribute AccountDepositRequest accountDepositRequest){
 
-
         try {
             accountManager.deposit(accountDepositRequest);
         }
         catch (IllegalArgumentException e) {
             // 입력값 에러
-            return "deposit/error-message/invalid-input-error";
+            return "redirect:/deposit/error/invalid-input-error";
         }
         catch(InvalidAccountException e) {
             // 존재하지 않는 계좌
-            return "deposit/error-message/invalid-account-error";
+            return "redirect:/deposit/error/invalid-account-error";
         }
         catch(Exception e) {
             // 예기치 못한 에러
-            return "deposit/error-message/unexpected-error";
+            return "redirect:/deposit/error/unexpected-error";
         }
 
         // 성공
+        return "redirect:/deposit/success/success?amount=" + accountDepositRequest.getAmount();
+    }
+
+    @GetMapping("/success/success")
+    public String depositSuccessPage(@RequestParam("amount") double amount , Model model){
+        AccountDepositRequest accountDepositRequest = new AccountDepositRequest(-1, amount);
+        model.addAttribute("accountDepositRequest", accountDepositRequest);
         return "deposit/success-message/success";
     }
 
+    @GetMapping("/error/invalid-input-error")
+    public String depositInvalidInputErrorPage(){
+        return "deposit/error-message/invalid-input-error";
+    }
 
+    @GetMapping("/error/invalid-account-error")
+    public String depositInvalidAccountErrorPage(){
+        return "deposit/error-message/invalid-account-error";
+    }
 
-
-
+    @GetMapping("/error/unexpected-error")
+    public String depositUnexpectedErrorPage(){
+        return "deposit/error-message/unexpected-error";
+    }
 }
